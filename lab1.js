48ba5540fd525b992c8e199b20ec10292ae61c72
jimenezkgabriel <jimenez.k.gabriel@gmail.com>
2025-11-07 12:37:40 -0500

    added task 2

--- lab1.js
+++ lab1.js
@@ -1,8 +1,6 @@
 function formatFullName(firstName, lastName) {
-    for (let i = 0; i < arguments.length; i++)
-    {
-        if (arguments[i] === "" || typeof arguments[i] != "string")
-        {
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] === "" || typeof arguments[i] != "string") {
             return "Invalid name input"
         }
     }
@@ -12,7 +10,7 @@ function formatFullName(firstName, lastName) {
     for (i = arguments.length - 1; i >= 0; i--) {
         arguments[i] = capitalize(arguments[i])
         newName = newName.concat(arguments[i], " ")
-    } 
+    }
 
     return newName.trim()
 }
@@ -22,3 +20,15 @@ let capitalize = (name) => {
 }
 
 console.log(formatFullName(`gabriel`, `jimenez`));
+
+let calculateTotalCost = (price, quantity, taxRate) => {
+    if (typeof price != "number") {
+        return "Invalid number";
+    }
+
+    let totalCost = (price * quantity) * (1 + taxRate);
+
+    return totalCost;
+}
+
+console.log(calculateTotalCost(2.99, 3, 0.03).toLocaleString("en-US", { style: "currency", currency: "USD" }));
